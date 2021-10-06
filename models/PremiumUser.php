8384f44b9a6080b80c7bd92f523a4c1ee6f84c92
Eliana-Abate <eliana.abate@outlook.it>
2021-10-06 16:28:17 +0200

    added props and function to premiumUser

---
+++ models/PremiumUser.php
@@ -0,0 +1,39 @@
+<?php require_once __DIR__ . '/User.php' ?> 
+
+<?php 
+
+class PremiumUser extends User
+{
+  protected $level;
+
+    protected function __construct($_name, $_surname, $_email, $_discount, $_level) {
+        
+        $this->level = $_level;
+        parent::__construct($_name, $_surname, $_email, $_discount);
+    }
+
+
+    private function setDiscount($_newsletter_subscribed) {
+
+        if ($this->level > 1 && $_newsletter_subscribed) {
+
+            $this->discount = 25;
+
+        } else if ($this->level > 1 && !$_newsletter_subscribed) {
+
+            $this->discount = 20;
+
+        } else {
+
+            $this->discount = 15;
+        }
+
+    }
+
+    protected function getDiscount() {
+        return $this->discount;
+    }
+
+   
+    
+}
