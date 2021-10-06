e225fd3832819dba4e4cffebf49eb64feed6d154
Eliana-Abate <eliana.abate@outlook.it>
2021-10-06 16:00:23 +0200

    added function

--- models/User.php
+++ models/User.php
@@ -5,16 +5,30 @@ class User
   protected $name;
   protected $surname;
   protected $email;
-  protected $newsletter_subscribed;
+  protected $discount;
 
 
 
-    public function __construct($_name, $_surname, $_email,  $_newsletter_subscribed)
+    public function __construct($_name, $_surname, $_email, $_discount)
     {
         $this->name = $_name;
         $this->surname = $_surname;
         $this->email = $_email;
-        $this->newsletter_subscribed = $_newsletter_subscribed;
+        $this->discount = $_discount;
+    }
+
+    private function setDiscount($_newsletter_subscribed) {
+
+        if ($_newsletter_subscribed) {
+            $this->discount = 10;
+        } else {
+            $this->discount = 0;
+        }
+
+    }
+
+    public function getDiscount() {
+        return $this->discount;
     }
 
     
